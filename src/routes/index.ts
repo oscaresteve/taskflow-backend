16c68d504e8e3c4d4742ee6968df969700467542
oscaresteve <oscarestevea24@gmail.com>
2026-07-30 15:23:53 +0200

    refactor(projects): create the base structure

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authRouter } from "../modules/auth/index.ts";
 import { workspacesRouter } from "../modules/workspaces/index.ts";
+import { projectsRouter } from "../modules/projects/index.ts";
 
 export const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/workspaces", workspacesRouter);
+router.use("/projects", projectsRouter);
