dca3d75c59955b8badf48b6126894809e48bafd5
oscaresteve <oscarestevea24@gmail.com>
2026-08-05 16:54:32 +0200

    fix(comments): capture errors on create

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -5,16 +5,20 @@ import * as commentsService from "./comments.service.ts";
 import { toCommentResponse } from "./mappers/comments.mapper.ts";
 
 export async function create(req: Request, res: Response, next: NextFunction) {
-  const userId = req.user.id;
-  const params = req.validated.params as TaskParamsDto;
-  const workspaceSlug = params.workspaceSlug;
-  const projectSlug = params.projectSlug;
-  const taskNumber = params.taskNumber;
-  const data = req.validated.body as CreateCommentDto;
+  try {
+    const userId = req.user.id;
+    const params = req.validated.params as TaskParamsDto;
+    const workspaceSlug = params.workspaceSlug;
+    const projectSlug = params.projectSlug;
+    const taskNumber = params.taskNumber;
+    const data = req.validated.body as CreateCommentDto;
 
-  const comment = await commentsService.create({ userId, workspaceSlug, projectSlug, taskNumber, data });
+    const comment = await commentsService.create({ userId, workspaceSlug, projectSlug, taskNumber, data });
 
-  const commentResponse = toCommentResponse(comment);
+    const commentResponse = toCommentResponse(comment);
 
-  res.status(201).json(commentResponse);
+    res.status(201).json(commentResponse);
+  } catch (error) {
+    next(error);
+  }
 }
