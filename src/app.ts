ff08bb45eed5419e066e275044d27ae43782c479
oscaresteve <oscarestevea24@gmail.com>
2026-08-06 19:36:53 +0200

    feat(cookie-parser): use cookie-parser in app

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import { router } from "./routes/index.ts";
 import { errorHandler } from "./shared/middlewares/error-handler.ts";
 import { env } from "./config/env.ts";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: env.CORS_ORIGIN }));
+app.use(cookieParser());
+
+app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+
 // TODO: Helmet
 // TODO: Logger
 
